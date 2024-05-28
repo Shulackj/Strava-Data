f00f51476a38ab1435b290a8a6e232e54b60dd82
Jacqueline Shulack <jackie@jacquelines-mbp.mynetworksettings.com>
2024-05-28 14:59:04 -0400

    Weekly Strava data pull

--- dags/get_strava_data.py
+++ dags/get_strava_data.py
@@ -12,9 +12,9 @@ import logging
 from airflow.operators.dummy_operator import DummyOperator
 
 def split_lat_lng(lat_lng_str):
-    if isinstance(lat_lng_str, list) and not lat_lng_str:  # If lat_lng_str is an empty list
-        return np.nan, np.nan  # Assign NaN or any other suitable value
-    elif isinstance(lat_lng_str, str):  # If lat_lng_str is a string
+    if isinstance(lat_lng_str, list) and not lat_lng_str:  
+        return np.nan, np.nan  
+    elif isinstance(lat_lng_str, str):  
         return lat_lng_str.split(',')
     else:
         return np.nan, np.nan
@@ -90,17 +90,14 @@ def get_new_strava_data(ti):
     
 
     #Columns of interest
-        #Columns of interest
     selected_fields = ['name', 'distance','moving_time', 'elapsed_time','gear_id', 'total_elevation_gain', 'type', 'sport_type', 'average_speed','max_speed', 'summary_polyline','start_date','timezone','elev_high','elev_low','average_heartrate', 'max_heartrate','average_cadence','start_latlng','end_latlng']
     summary_polyline = [activity['map']['summary_polyline'] for activity in all_activities]
 
 
     df = pd.DataFrame([{field: activity.get(field) for field in selected_fields} for activity in all_activities])
     df['summary_polyline'] = summary_polyline
-    #convert gear_id to string
-    
-   
     df['gear_id'] = df['gear_id'].astype(str)
+
     if 'start_latlng' in df.columns:
         # Splitting 'start_latlng' into separate columns for latitude and longitude
         df['start_lat'], df['start_lng'] = zip(*df['start_latlng'].apply(split_lat_lng))
@@ -117,65 +114,36 @@ def get_new_strava_data(ti):
   
 
 def clean_data(ti):
-    #pull in new data
     df = ti.xcom_pull(task_ids='get_strava_data', key='new_data')
    
-    #shorten date
     df['start_date'] = pd.to_datetime(df['start_date'])
-
-    # Extract the date part as a string
     df['start_date'] = df['start_date'].dt.strftime("%Y-%m-%d")
-
-    #convert distance in meters to miles
     df['distance'] = (df['distance'].fillna(0) * 0.000621371).astype(float)
-
-    #time is in s,coverting to mins
     df['moving_time'] = df['moving_time']/60
     df['elapsed_time'] = df['elapsed_time']/60
-
-    # get the hours and mins
     df['moving_hours'], df['moving_mins'] = divmod(df['moving_time'] , 60)
     df['elapsed_hours'], df['elapsed_mins'] = divmod(df['elapsed_time'] , 60)
-
-    #total time in mins
     df['total_moving_time'] = df['moving_hours']*60 + df['moving_mins']
     df['total_elapsed_time'] = df['elapsed_hours']*60 + df['elapsed_mins']
-
-    #Speed is in m/s, coverting to  miles/hour
     df['average_speed'] = round((df['average_speed'] * 2.23694),1)
     df['max_speed'] = round((df['max_speed'] * 2.23694),1)
-
-    # defining average pace per miles
     df['pace'] = df['total_elapsed_time']/df['distance']
-
-    #Get mins and seconds for mile pace
     df['average_pace_mins'] ,df['average_pace_secs']=  divmod(df['pace']*60,60)
-    # Round to prevent infinite numbers
     df['average_pace_mins'] = df['average_pace_mins'].round()
     df['average_pace_secs'] = df['average_pace_secs'].round()
-   
-    #Replace infinite,nan values
     df = df.replace([np.inf, -np.inf, np.nan], 0)
     df['average_pace_mins'] = df['average_pace_mins'].astype(int)
     df['average_pace_secs'] = df['average_pace_secs'].astype(int)
 
-    #pad seconds
     df['average_pace_secs'] = df['average_pace_secs'].astype(str).str.zfill(2)
-    #Calculate the average mins and second pace
     df['average_pace'] = df['average_pace_mins'].astype(str) + ':' + df['average_pace_secs']
     df = df.drop(labels=['pace', 'average_pace_mins','average_pace_secs'], axis=1)
     ti.xcom_push(key='cleaned_data', value=df)
 
 def concatenate_data(ti):
-    #Read the uploaded file
-    print("Files in directory:")
-    print(os.listdir('/opt/airflow/dags'))
-
-    # Read the uploaded file
     file_path = '/opt/airflow/dags/Strava_update.csv'
     df_uploaded = pd.read_csv(file_path)
-    # print(len(df_uploaded))
-    # Retrieve the new Strava data from XCom
+  
     df_strava = ti.xcom_pull(task_ids='clean_new_data', key='cleaned_data')
    
     # Concatenate the data
